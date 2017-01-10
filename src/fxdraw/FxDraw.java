b1aede6f1cc883e0851499667164d1e64cf533ab
Sandro Gallo <gallo.sandro@gmail.com>
2017-01-10 16:59:56 +0100

    Version Advanced

--- src/fxdraw/FxDraw.java
+++ src/fxdraw/FxDraw.java
@@ -18,7 +18,7 @@ import javafx.stage.Stage;
  */
 public class FxDraw extends Application {
   
-  public final int w=1024, h=768;
+  public final int w=600, h=600;
   
   @Override
   public void start(Stage stage) {
@@ -31,6 +31,20 @@ public class FxDraw extends Application {
       gc.setStroke(Color.BLUE);
       gc.strokeLine(10, 10, w-10, h-10);
       gc.strokeLine(10, h-10, w-10, 10);
+      
+      gc.setLineWidth(1);
+      gc.setStroke(Color.RED);
+      for (int i = 10; i < 200; i+=5) {
+        gc.strokeLine(i, i, w-i, i);
+        gc.strokeLine(w-i, i, w-i, h-i);
+        gc.strokeLine(w-i, h-i, i, h-i);
+        gc.strokeLine(i, h-i, i, i);
+      }
+      gc.setLineWidth(2);
+      for (int i = 0; i < 60; i+=5) {
+        gc.setStroke( Color.rgb( (int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256) ) );
+        gc.strokeOval(220+i, 220+i, 100, 100);
+      }
 
       root.getChildren().add(canvas);
       stage.setScene(new Scene(root));
